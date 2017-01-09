8baad190ada755f5644f7a66124fa0ce73cca760
Noah Gleason <noah.gleason1@gmail.com>
2017-01-08 22:15:18 -0500

    Added a TalonClusterDrive with a DefaultDrive.

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -1,9 +1,12 @@
 package org.usfirst.frc.team449.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.usfirst.frc.team449.robot.drive.talonCluster.TalonClusterDrive;
 
 /**
  * Created by BlairRobot on 2017-01-08.
  */
 public class Robot extends IterativeRobot {
+
+    public static TalonClusterDrive driveSubsystem;
 }
---
+++ src/main/java/org/usfirst/frc/team449/robot/drive/talonCluster/TalonClusterDrive.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team449.robot.drive.talonCluster;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import maps.org.usfirst.frc.team449.robot.components.CANTalonSRXMap;
+import org.usfirst.frc.team449.robot.components.CANTalonSRX;
+import org.usfirst.frc.team449.robot.drive.DriveSubsystem;
+
+/**
+ * Created by BlairRobot on 2017-01-08.
+ */
+public class TalonClusterDrive extends DriveSubsystem {
+
+	public CANTalonSRX rightMaster;
+	public CANTalonSRX leftMaster;
+
+	public TalonClusterDrive(maps.org.usfirst.frc.team449.robot.drive.talonCluster.TalonClusterDriveMap.TalonClusterDrive map){
+		super(map.getDrive());
+		rightMaster = new CANTalonSRX(map.getRightMaster());
+		leftMaster = new CANTalonSRX(map.getLeftMaster());
+		for (CANTalonSRXMap.CANTalonSRX talon : map.getRightSlaveList()){
+			CANTalonSRX talonObject = new CANTalonSRX(talon);
+			talonObject.canTalon.changeControlMode(CANTalon.TalonControlMode.Follower);
+			talonObject.canTalon.set(map.getRightMaster().getPort());
+		}
+		for (CANTalonSRXMap.CANTalonSRX talon : map.getLeftSlaveList()){
+			CANTalonSRX talonObject = new CANTalonSRX(talon);
+			talonObject.canTalon.changeControlMode(CANTalon.TalonControlMode.Follower);
+			talonObject.canTalon.set(map.getLeftMaster().getPort());
+		}
+	}
+
+	/**
+	 * Sets the left and right wheel speeds as a voltage percentage, not nearly as precise as PID.
+	 * @param left The left throttle, a number between -1 and 1 inclusive.
+	 * @param right The right throttle, a number between -1 and 1 inclusive.
+	 */
+	public void setVBusThrottle(double left, double right){
+		leftMaster.setPercentVbus(left);
+		rightMaster.setPercentVbus(right);
+	}
+
+	/**
+	 * Allows the type of motor control used to be varied in testing.
+	 * @param left Left throttle value
+	 * @param right Right throttle value
+	 */
+	public void setDefaultThrottle(double left, double right){
+		setVBusThrottle(left, right);
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+
+	}
+}
