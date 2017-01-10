abe6663a2c326b77a786901a45a3de956962ba4c
Noah Gleason <noah.gleason1@gmail.com>
2017-01-10 15:58:55 -0500

    Ready to test.

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -1,9 +1,32 @@
 package org.usfirst.frc.team449.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import maps.org.usfirst.frc.team449.robot.Robot2017Map;
+import org.usfirst.frc.team449.robot.mechanism.doubleflywheelshooter.DoubleFlywheelShooter;
+import org.usfirst.frc.team449.robot.oi.OI2017;
+
+import java.io.IOException;
 
 /**
  * Created by BlairRobot on 2017-01-08.
  */
 public class Robot extends IterativeRobot {
+
+	public static DoubleFlywheelShooter shooterSubsystem;
+
+	public static OI2017 oi;
+
+	public static maps.org.usfirst.frc.team449.robot.Robot2017Map.Robot2017 cfg;
+
+	public void robotInit(){
+		try {
+			cfg = (Robot2017Map.Robot2017) MappedSubsystem.readConfig("/home/lvuser/map.cfg", Robot2017Map.Robot2017.newBuilder());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		oi = new OI2017(cfg.getOi());
+		shooterSubsystem = new DoubleFlywheelShooter(cfg.getShooter());
+
+		oi.mapButtons();
+	}
 }
--- src/main/java/org/usfirst/frc/team449/robot/oi/OI2017.java
+++ src/main/java/org/usfirst/frc/team449/robot/oi/OI2017.java
@@ -1,7 +1,46 @@
 package org.usfirst.frc.team449.robot.oi;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.usfirst.frc.team449.robot.Robot;
+import org.usfirst.frc.team449.robot.mechanism.doubleflywheelshooter.commands.ToggleFlywheel;
+
 /**
  * Created by blairrobot on 1/10/17.
  */
-public class OI2017 {
+public class OI2017 extends OISubsystem{
+
+	private Joystick buttonPad;
+	private Button toggleFlywheel;
+
+	public OI2017(maps.org.usfirst.frc.team449.robot.oi.OI2017Map.OI2017 map){
+		super(map.getOi());
+		buttonPad = new Joystick(map.getButtonPad());
+		toggleFlywheel = new JoystickButton(buttonPad, map.getToggleFlywheel());
+	}
+
+	public void mapButtons(){
+		toggleFlywheel.whenPressed(new ToggleFlywheel(Robot.shooterSubsystem));
+	}
+
+	@Override
+	public double getDriveAxisLeft() {
+		return 0; //Do Nothing!
+	}
+
+	@Override
+	public double getDriveAxisRight() {
+		return 0; //Do Nothing!
+	}
+
+	@Override
+	public void toggleCamera() {
+		//Do Nothing!
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		//Do Nothing!
+	}
 }
