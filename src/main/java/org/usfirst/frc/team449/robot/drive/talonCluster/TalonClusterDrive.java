3e0f5e46f45fb3b679db59e5c1a4cb59bf13cbb7
Noah Gleason <noah.gleason1@gmail.com>
2017-01-11 18:13:25 -0500

    Working unit conversions and F value.

--- src/main/java/org/usfirst/frc/team449/robot/drive/talonCluster/TalonClusterDrive.java
+++ src/main/java/org/usfirst/frc/team449/robot/drive/talonCluster/TalonClusterDrive.java
@@ -5,13 +5,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import maps.org.usfirst.frc.team449.robot.components.CANTalonSRXMap;
 import org.usfirst.frc.team449.robot.components.CANTalonSRX;
 import org.usfirst.frc.team449.robot.drive.DriveSubsystem;
-import org.usfirst.frc.team449.robot.drive.talonCluster.commands.DefaultDrive;
 import org.usfirst.frc.team449.robot.drive.talonCluster.commands.DriveStraight;
 import org.usfirst.frc.team449.robot.oi.OI2017;
-import org.usfirst.frc.team449.robot.oi.OISubsystem;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by BlairRobot on 2017-01-08.
@@ -22,6 +21,9 @@ public class TalonClusterDrive extends DriveSubsystem {
 	public CANTalonSRX leftMaster;
 
 	private long startTime;
+
+	private double rightMapF;
+	private double leftMapF;
 
 	public OI2017 oi;
 
@@ -41,6 +43,7 @@ public class TalonClusterDrive extends DriveSubsystem {
 			talonObject.canTalon.changeControlMode(CANTalon.TalonControlMode.Follower);
 			talonObject.canTalon.set(map.getLeftMaster().getPort());
 		}
+		setPIDF();
 	}
 
 	/**
@@ -54,8 +57,8 @@ public class TalonClusterDrive extends DriveSubsystem {
 	}
 
 	public void setPIDThrottle(double left, double right){
-		leftMaster.setSpeed(1023/leftMaster.canTalon.getF()/60*left);
-		rightMaster.setSpeed(1023/rightMaster.canTalon.getF()/60*right);
+		leftMaster.setSpeed(RPSToNative(left*leftMapF)/60);
+		rightMaster.setSpeed(RPSToNative(right*rightMapF)/60);
 	}
 
 	/**
@@ -64,7 +67,7 @@ public class TalonClusterDrive extends DriveSubsystem {
 	 * @param right Right throttle value
 	 */
 	public void setDefaultThrottle(double left, double right){
-		setVBusThrottle(left, right);
+		setPIDThrottle(left, right);
 	}
 
 	public static double nativeToRPS(double nativeUnits){
@@ -75,19 +78,35 @@ public class TalonClusterDrive extends DriveSubsystem {
 		return (rps/10)*(512*4); //512 Counts per revolution, 4 edges per count, 10 100ms per second.
 	}
 
+	public void setPIDF(){
+		rightMapF = rightMaster.canTalon.getF();
+		leftMapF = leftMaster.canTalon.getF();
+		rightMaster.canTalon.setF(1023/RPSToNative(rightMapF));
+		leftMaster.canTalon.setF(1023/RPSToNative(leftMapF));
+	}
+
 	public void logData(double throttle){
-		try (FileWriter fw = new FileWriter("/home/lvuser/shooterLog.csv", true)) {
+		try (FileWriter fw = new FileWriter("/home/lvuser/driveLog.csv", true)) {
 			StringBuilder sb = new StringBuilder();
 			sb.append((System.nanoTime()-startTime)/100);
 			sb.append(",");
-			sb.append(leftMaster.canTalon.getEncVelocity());
+			sb.append(nativeToRPS(leftMaster.canTalon.getEncVelocity()));
 			sb.append(",");
-			SmartDashboard.putNumber("Left",leftMaster.canTalon.getEncVelocity());
-			sb.append(rightMaster.canTalon.getEncVelocity());
+			SmartDashboard.putNumber("Left", nativeToRPS(leftMaster.canTalon.getEncVelocity()));
+			sb.append(nativeToRPS(rightMaster.canTalon.getEncVelocity()));
 			sb.append(",");
-			SmartDashboard.putNumber("Right", rightMaster.canTalon.getEncVelocity());
+			SmartDashboard.putNumber("Right", nativeToRPS(rightMaster.canTalon.getEncVelocity()));
 			sb.append(throttle);
 			sb.append("\n");
+			SmartDashboard.putNumber("Throttle", throttle);
+			SmartDashboard.putNumber("Left Setpoint", leftMaster.canTalon.getSetpoint());
+			SmartDashboard.putNumber("Left Error", leftMaster.canTalon.getError());
+			SmartDashboard.putNumber("Right Setpoint", rightMaster.canTalon.getSetpoint());
+			SmartDashboard.putNumber("Right Error", rightMaster.canTalon.getError());
+			SmartDashboard.putNumber("Left F", leftMaster.canTalon.getF());
+			SmartDashboard.putNumber("Right F", rightMaster.canTalon.getF());
+			SmartDashboard.putNumber("Right voltage",rightMaster.canTalon.getOutputVoltage());
+			SmartDashboard.putNumber("Left voltage",leftMaster.canTalon.getOutputVoltage());
 			fw.write(sb.toString());
 		}catch (IOException e){
 			e.printStackTrace();
@@ -96,6 +115,12 @@ public class TalonClusterDrive extends DriveSubsystem {
 
 	@Override
 	protected void initDefaultCommand() {
+		try (PrintWriter writer = new PrintWriter("/home/lvuser/driveLog.csv")) {
+			writer.println("Time,Left,Right,Throttle");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		startTime = System.nanoTime();
 		setDefaultCommand(new DriveStraight(this, oi));
 	}
