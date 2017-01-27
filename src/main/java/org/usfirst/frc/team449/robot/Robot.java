07da7d360bcb7ad64b8944be080bc09aa26fae45
Noah Gleason <noah.gleason1@gmail.com>
2017-01-27 11:19:50 -0500

    Add gradle task to copy resources.

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -32,7 +32,7 @@ public class Robot extends IterativeRobot {
 	public void robotInit() {
 		System.out.println("Started robotInit");
 		try {
-			cfg = (Robot2017Map.Robot2017) MappedSubsystem.readConfig("/home/lvuser/map.cfg", Robot2017Map.Robot2017.newBuilder());
+			cfg = (Robot2017Map.Robot2017) MappedSubsystem.readConfig("/home/lvuser/449_resources/map.cfg", Robot2017Map.Robot2017.newBuilder());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
