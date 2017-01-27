d6c6048e181108d32ce02d5a3e0114e042d801c9
Ryan Tse <rytse@mbhs.edu>
2017-01-26 19:02:40 -0500

    Import necessary commands in OI2017 class

--- src/main/java/org/usfirst/frc/team449/robot/oi/OI2017.java
+++ src/main/java/org/usfirst/frc/team449/robot/oi/OI2017.java
@@ -7,6 +7,7 @@ import org.usfirst.frc.team449.robot.Robot;
 import org.usfirst.frc.team449.robot.drive.talonCluster.commands.NavXDriveStraight;
 import org.usfirst.frc.team449.robot.drive.talonCluster.commands.NavXRelativeTTA;
 import org.usfirst.frc.team449.robot.drive.talonCluster.commands.NavXTurnToAngle;
+import org.usfirst.frc.team449.robot.mechanism.climber.commands.Climb;
 import org.usfirst.frc.team449.robot.oi.components.PolyThrottle;
 import org.usfirst.frc.team449.robot.oi.components.Throttle;
 
@@ -44,10 +45,10 @@ public class OI2017 extends OISubsystem {
 		buttonPad = new Joystick(map.getButtonPad());
 		leftThrottle = new PolyThrottle(leftStick, 1, 1);
 		rightThrottle = new PolyThrottle(rightStick, 1, 1);
-//		leftThrottle = new SmoothedThrottle(leftStick, 1);
-//		rightThrottle = new SmoothedThrottle(rightStick, 1);
-//		leftThrottle = new ExpThrottle(leftStick, 1, 50);
-//		rightThrottle = new ExpThrottle(rightStick, 1, 50);
+		//		leftThrottle = new SmoothedThrottle(leftStick, 1);
+		//		rightThrottle = new SmoothedThrottle(rightStick, 1);
+		//		leftThrottle = new ExpThrottle(leftStick, 1, 50);
+		//		rightThrottle = new ExpThrottle(rightStick, 1, 50);
 		turnaround = new JoystickButton(leftStick, map.getTurnaroundButton());
 		tt0 = new JoystickButton(leftStick, map.getTurnTo0Button());
 		tt30 = new JoystickButton(leftStick, map.getTurnTo30Button());
@@ -66,8 +67,10 @@ public class OI2017 extends OISubsystem {
 		tt330.whenPressed(new NavXTurnToAngle(Robot.driveSubsystem.turnPID, -30, Robot.driveSubsystem, 2.5));
 		driveStraight.whileHeld(new NavXDriveStraight(Robot.driveSubsystem.straightPID, Robot.driveSubsystem, this));
 		climbButton.whileHeld(new Climb(Robot.climberSubsystem));
-		toggleFlywheel.whenPressed(new ToggleFlywheel(Robot.doubleFlywheelShooterSubsystem));
-		toggleFlywheel.whenPressed(new ToggleFlywheel(Robot.singleFlywheelShooterSubsystem));
+		toggleFlywheel.whenPressed(new org.usfirst.frc.team449.robot.mechanism.doubleflywheelshooter.commands
+				.ToggleFlywheel(Robot.doubleFlywheelShooterSubsystem));
+		toggleFlywheel.whenPressed(new org.usfirst.frc.team449.robot.mechanism.singleflywheelshooter.commands
+				.ToggleFlywheel(Robot.singleFlywheelShooterSubsystem));
 	}
 
 	@Override
