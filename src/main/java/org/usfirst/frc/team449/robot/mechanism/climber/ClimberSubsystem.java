cc331c53012ca4cfe47ce952f9df6b1e23302195
Ryan Tse <rytse@mbhs.edu>
2017-01-29 15:32:03 -0500

    Remove OI from ClimberSubsystem

--- src/main/java/org/usfirst/frc/team449/robot/mechanism/climber/ClimberSubsystem.java
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/climber/ClimberSubsystem.java
@@ -2,21 +2,17 @@ package org.usfirst.frc.team449.robot.mechanism.climber;
 
 import org.usfirst.frc.team449.robot.components.CANTalonSRX;
 import org.usfirst.frc.team449.robot.mechanism.MechanismSubsystem;
-import org.usfirst.frc.team449.robot.oi.OI2017;
 
 /**
  * Created by Justin on 1/12/2017.
  */
 public class ClimberSubsystem extends MechanismSubsystem {
-
 	CANTalonSRX canTalonSRX;
-	OI2017 oi;
 
-	public ClimberSubsystem(maps.org.usfirst.frc.team449.robot.mechanism.climber.ClimberMap.Climber map, OI2017 oi) {
+	public ClimberSubsystem(maps.org.usfirst.frc.team449.robot.mechanism.climber.ClimberMap.Climber map) {
 		super(map.getMechanism());
 		this.map = map;
 		canTalonSRX = new CANTalonSRX(map.getWinch());
-		this.oi = oi;
 	}
 
 	@Override
