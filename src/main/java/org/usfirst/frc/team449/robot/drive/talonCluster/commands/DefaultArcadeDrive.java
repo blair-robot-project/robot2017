972ec80335575fac4b4094d85cd7e67b1877b03c
Noah Gleason <noah.gleason1@gmail.com>
2017-02-05 18:20:53 -0500

    Comment DefaultArcadeDrive.java.

--- src/main/java/org/usfirst/frc/team449/robot/drive/talonCluster/commands/DefaultArcadeDrive.java
+++ src/main/java/org/usfirst/frc/team449/robot/drive/talonCluster/commands/DefaultArcadeDrive.java
@@ -4,20 +4,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import maps.org.usfirst.frc.team449.robot.components.ToleranceBufferAnglePIDMap;
 import org.usfirst.frc.team449.robot.components.PIDAngleCommand;
 import org.usfirst.frc.team449.robot.drive.talonCluster.TalonClusterDrive;
+import org.usfirst.frc.team449.robot.drive.talonCluster.commands.ois.ArcadeOI;
 import org.usfirst.frc.team449.robot.oi.OI2017ArcadeGamepad;
 
 /**
  * Created by Noah Gleason on 1/30/2017.
  */
 public class DefaultArcadeDrive extends PIDAngleCommand{
-	public OI2017ArcadeGamepad oi;
-	
+	//The OI giving the vel and turn stick values.
+	public ArcadeOI oi;
+
+	//Whether or not we should be using the NavX to drive straight stably.
 	private boolean drivingStraight;
+	//The velocity input from OI. Should be between -1 and 1.
 	private double vel;
+	//The rotation input from OI. Should be between -1 and 1.
 	private double rot;
+	//The talonClusterDrive this command is controlling.
 	private TalonClusterDrive driveSubsystem;
 
-	public DefaultArcadeDrive(ToleranceBufferAnglePIDMap.ToleranceBufferAnglePID map, TalonClusterDrive drive, OI2017ArcadeGamepad oi) {
+	public DefaultArcadeDrive(ToleranceBufferAnglePIDMap.ToleranceBufferAnglePID map, TalonClusterDrive drive, ArcadeOI oi) {
 		super(map, drive);
 		this.oi = oi;
 		requires(drive);
@@ -27,35 +33,49 @@ public class DefaultArcadeDrive extends PIDAngleCommand{
 
 	@Override
 	protected void initialize() {
+		//Reset all values of the PIDController and disable it.
 		this.getPIDController().reset();
 		System.out.println("DefaultArcadeDrive init.");
+		//Initial assignment
 		drivingStraight = false;
 		vel = oi.getFwd();
-		driveSubsystem.setDefaultThrottle(0.0, 0.0);
+		rot = oi.getRot();
 	}
 
 	@Override
 	protected void execute() {
-		if (drivingStraight && oi.getRot() != 0){
+		//Set vel and rot to what they should be.
+		vel = oi.getFwd();
+		rot = oi.getRot();
+
+		//If we're driving straight but the driver tries to turn:
+		if (drivingStraight && rot != 0){
+			//Switch to free drive
 			drivingStraight = false;
-			//Reset disables it too.
+			//Reset and disable the PID loop.
 			this.getPIDController().reset();
 			System.out.println("Switching to free drive.");
-		} else if (!(drivingStraight) && oi.getRot() == 0){
+		}
+		//If we're free driving and the driver lets go of the turn stick:
+		else if (!(drivingStraight) && rot == 0){
+			//Switch to driving straight
 			drivingStraight = true;
+			//Set the setpoint to the current heading
 			this.getPIDController().setSetpoint(subsystem.getGyroOutput());
+			//Enable the controller
 			this.getPIDController().enable();
 			System.out.println("Switching to DriveStraight.");
 		}
+
+		//Log data and stuff
 		driveSubsystem.logData();
-		vel = oi.getFwd();
-		rot = oi.getRot();
 		SmartDashboard.putBoolean("driving straight?", drivingStraight);
 		SmartDashboard.putNumber("Vel Axis", vel);
 	}
 
 	@Override
 	protected boolean isFinished() {
+		//Runs constantly because this is a defaultDrive
 		return false;
 	}
 
@@ -72,7 +92,9 @@ public class DefaultArcadeDrive extends PIDAngleCommand{
 
 	@Override
 	protected void usePIDOutput(double output) {
+		//If we're driving straight..
 		if (drivingStraight) {
+			//If we're using minimumOutput..
 			if (minimumOutputEnabled) {
 				//Set the output to the minimum if it's too small.
 				if (output > 0 && output < minimumOutput) {
@@ -81,13 +103,20 @@ public class DefaultArcadeDrive extends PIDAngleCommand{
 					output = -minimumOutput;
 				}
 			}
+			//Set the output to 0 if we're within the deadband. Whether or not the deadband is enabled is dealt with in PIDAngleCommand.
 			if (Math.abs(this.getPIDController().getError()) < deadband) {
 				output = 0;
 			}
+			//Log stuff
 			SmartDashboard.putNumber("PID output", output);
+
+			//Adjust the heading according to the PID output, it'll be positive if we want to go right.
 			driveSubsystem.setDefaultThrottle(vel + output, vel - output);
-		} else {
-			driveSubsystem.setDefaultThrottle(vel + rot, vel - rot);
+		}
+		//If we're free driving...
+		else {
+			//Set the throttle to normal arcade throttle.
+			driveSubsystem.setDefaultThrottle(vel - rot, vel + rot);
 		}
 	}
 }
