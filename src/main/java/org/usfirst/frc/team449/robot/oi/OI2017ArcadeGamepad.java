1959a6745b824e60a4c619979964dff031454a78
Ryan Tse <rytse@mbhs.edu>
2017-02-19 14:38:10 -0500

    Finalize shift up on high gear

--- src/main/java/org/usfirst/frc/team449/robot/oi/OI2017ArcadeGamepad.java
+++ src/main/java/org/usfirst/frc/team449/robot/oi/OI2017ArcadeGamepad.java
@@ -110,6 +110,9 @@ public class OI2017ArcadeGamepad extends BaseOI implements ArcadeOI {
 		if (map.hasFireShooter()) {
 			fireShooter = new MJButton(map.getFireShooter());
 		}
+		if (map.hasSwitchCamera()){
+			switchCamera = new MJButton(map.getSwitchCamera());
+		}
 	}
 
 	/**
