b266a1c6b9ac8549d75549984ee2fab8dddf4d14
Ryan Tse <rytse@mbhs.edu>
2017-02-18 23:14:14 -0500

    Add top-level commands for loading, racking, and firing the shooter

---
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/topcommands/shooter/FireShooter.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team449.robot.mechanism.topcommands.shooter;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team449.robot.mechanism.feeder.FeederSubsystem;
+import org.usfirst.frc.team449.robot.mechanism.feeder.commands.RunFeeder;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.Intake2017;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.spin.DynamicIntakeStop;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.spin.StaticIntakeIn;
+import org.usfirst.frc.team449.robot.mechanism.singleflywheelshooter.SingleFlywheelShooter;
+import org.usfirst.frc.team449.robot.mechanism.singleflywheelshooter.commands.AccelerateFlywheel;
+
+/**
+ * Created by ryant on 2017-02-18.
+ */
+public class FireShooter extends CommandGroup {
+	public FireShooter(SingleFlywheelShooter sfs, Intake2017 intake, FeederSubsystem feeder) {
+		addParallel(new AccelerateFlywheel(sfs, 2.5 * 60)); // TODO remove useless timeout
+
+		addParallel(new StaticIntakeIn(intake));
+		addParallel(new DynamicIntakeStop(intake));
+
+		addParallel(new RunFeeder(feeder));
+	}
+}
---
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/topcommands/shooter/RackShooter.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team449.robot.mechanism.topcommands.shooter;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team449.robot.mechanism.feeder.FeederSubsystem;
+import org.usfirst.frc.team449.robot.mechanism.feeder.commands.RunFeeder;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.Intake2017;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.spin.DynamicIntakeIn;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.spin.StaticIntakeStop;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.updown.IntakeUp;
+import org.usfirst.frc.team449.robot.mechanism.singleflywheelshooter.SingleFlywheelShooter;
+import org.usfirst.frc.team449.robot.mechanism.singleflywheelshooter.commands.DecelerateFlywheel;
+
+/**
+ * Created by ryant on 2017-02-18.
+ */
+public class RackShooter extends CommandGroup {
+	public RackShooter(SingleFlywheelShooter sfs, Intake2017 intake, FeederSubsystem feeder) {
+		addParallel(new DecelerateFlywheel(sfs, 5));
+
+		addParallel(new IntakeUp(intake));
+		addParallel(new StaticIntakeStop(intake));
+		addParallel(new DynamicIntakeIn(intake));
+
+		addParallel(new RunFeeder(feeder));
+	}
+}
