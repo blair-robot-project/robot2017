b039be36570212cc4c1ddfd36806af516fd7b0f0
Ryan Tse <rytse@mbhs.edu>
2017-02-18 22:56:56 -0500

    Organize in and stop commands

---
+++ src/main/java/org/usfirst/frc/team449/robot/mechanism/intake/Intake2017/commands/spin/DynamicIntakeIn.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.commands.spin;
+
+import org.usfirst.frc.team449.robot.ReferencingCommand;
+import org.usfirst.frc.team449.robot.mechanism.intake.Intake2017.Intake2017;
+
+/**
+ * Created by ryant on 2017-02-18.
+ */
+public class DynamicIntakeIn extends ReferencingCommand {
+	Intake2017 intake;
+
+	public DynamicIntakeIn(Intake2017 intake) {
+		super(intake);
+		this.intake = intake;
+	}
+
+	@Override
+	protected void initialize() {
+		intake.setActuatedVictor(1);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		intake.setActuatedVictor(0);
+	}
+
+	@Override
+	protected void interrupted() {
+		intake.setActuatedVictor(0);
+	}
+}
