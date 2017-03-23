f2b296a2a59ee94613a3ef5b26cc8d938f1a78a4
Noah Gleason <noah.gleason1@gmail.com>
2017-03-23 17:42:25 -0400

    Generate all profiles simultaneously.

--- Pathgen/src/main/java/org/usfirst/frc/team449/pathgen/Pathgen.java
+++ Pathgen/src/main/java/org/usfirst/frc/team449/pathgen/Pathgen.java
@@ -7,6 +7,8 @@ import jaci.pathfinder.modifiers.TankModifier;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Generates a motion profile that hits any number of waypoints.
@@ -14,8 +16,8 @@ import java.io.IOException;
 public class Pathgen {
 	public static void main(String[] args) throws IOException {
 		final double CENTER_TO_FRONT = 27./2.;
-		final double CENTER_TO_BACK = 27./2.;
-		final double CENTER_TO_SIDE = 29./2;
+		final double CENTER_TO_BACK = 27./2. + 3.;
+		final double CENTER_TO_SIDE = 29./2. + 3.5;
 		final double BACK_FROM_PEG = 3;
 		//DO NOT TOUCH THE ONES BELOW
 		final double CARRIAGE_LEN = 3.63;
@@ -29,7 +31,7 @@ public class Pathgen {
 
 		Waypoint[] points = new Waypoint[]{ //Units are feet and radians.
 				new Waypoint(0, 0, 0),
-				new Waypoint(50./12., 0, 0)
+				new Waypoint(100./12., 0, 0)
 		};
 
 		Waypoint[] left = new Waypoint[]{
@@ -82,10 +84,16 @@ public class Pathgen {
 				new Waypoint(15,0,0)
 		};
 
-		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH,
-				0.05, 6.3, 1.5, 6); //Units are seconds, feet/second, feet/(second^2), and feet/(second^3)
+		Map<String, Waypoint[]> profiles = new HashMap<>();
+		profiles.put("Left", left);
+		profiles.put("Right", right);
+		profiles.put("Mid", center);
+		profiles.put("RedShoot", redPegToKey);
+		profiles.put("BlueShoot", bluePegToKey);
+		profiles.put("RedBackup", backupRed);
+		profiles.put("BlueBackup", backupBlue);
 
-		Trajectory trajectory = Pathfinder.generate(forward, config);
+		final String ROBOT_NAME = "calcifer";
 
 		//Calculated by driving each wheel n inches in opposite directions, then taking the angle moved, θ, and finding
 		// the circumference of a circle moved by the robot via C = 360 * n / θ
@@ -94,27 +102,38 @@ public class Pathgen {
 		//200 in: 29.96
 		//50 in: 34.2
 
-		TankModifier tm = new TankModifier(trajectory).modify(balbasaurWheelbase); //Units are feet
+		double calciferWheelbase = 35.3/12.;
 
-		FileWriter lfw = new FileWriter("forwardProfile.csv", false);
-		FileWriter rfw = new FileWriter("balbasaurRightBackupRedProfile.csv", false);
+		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH,
+				0.05, 6.3, 1.5, 6); //Units are seconds, feet/second, feet/(second^2), and feet/(second^3)
+
+		for (String profile : profiles.keySet()) {
+			Trajectory trajectory = Pathfinder.generate(profiles.get(profile), config);
+
+			TankModifier tm = new TankModifier(trajectory).modify(calciferWheelbase); //Units are feet
+
+			FileWriter lfw = new FileWriter(ROBOT_NAME+"Left"+profile+"Profile.csv", false);
+			FileWriter rfw = new FileWriter(ROBOT_NAME+"Right"+profile+"Profile.csv", false);
 
 
-		for (int i = 0; i < tm.getLeftTrajectory().length(); i++) {
-			lfw.write(tm.getLeftTrajectory().get(i).position + ",\t" + tm.getLeftTrajectory().get(i).velocity + ",\t"
-					+ tm.getLeftTrajectory().get(i).dt + ",");
-			lfw.write("\n");
+			lfw.write(tm.getLeftTrajectory().length()+"\n");
+			for (int i = 0; i < tm.getLeftTrajectory().length(); i++) {
+				lfw.write(tm.getLeftTrajectory().get(i).position + ",\t" + tm.getLeftTrajectory().get(i).velocity + ",\t"
+						+ tm.getLeftTrajectory().get(i).dt + ",");
+				lfw.write("\n");
+			}
+
+			rfw.write(tm.getRightTrajectory().length()+"\n");
+			for (int i = 0; i < tm.getRightTrajectory().length(); i++) {
+				rfw.write(tm.getRightTrajectory().get(i).position + ",\t" + tm.getRightTrajectory().get(i).velocity + "," +
+						"\t" + tm.getRightTrajectory().get(i).dt + ",");
+				rfw.write("\n");
+			}
+
+			lfw.flush();
+			lfw.close();
+			rfw.flush();
+			rfw.close();
 		}
-
-		for (int i = 0; i < tm.getRightTrajectory().length(); i++) {
-			rfw.write(tm.getRightTrajectory().get(i).position + ",\t" + tm.getRightTrajectory().get(i).velocity + "," +
-					"\t" + tm.getRightTrajectory().get(i).dt + ",");
-			rfw.write("\n");
-		}
-
-		lfw.flush();
-		lfw.close();
-		rfw.flush();
-		rfw.close();
 	}
 }
