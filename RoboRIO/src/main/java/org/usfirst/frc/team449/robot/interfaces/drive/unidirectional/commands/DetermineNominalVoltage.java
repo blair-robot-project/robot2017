ef3f27b91ffc2db69942236e11f01f14c59fc0c0
Noah Gleason <noah.gleason1@gmail.com>
2017-06-24 20:27:11 -0400

    Make command to determine nominal voltage.

---
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/interfaces/drive/unidirectional/commands/DetermineNominalVoltage.java
@@ -0,0 +1,86 @@
+package org.usfirst.frc.team449.robot.interfaces.drive.unidirectional.commands;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.usfirst.frc.team449.robot.interfaces.drive.unidirectional.UnidirectionalDrive;
+import org.usfirst.frc.team449.robot.util.Logger;
+import org.usfirst.frc.team449.robot.util.YamlCommandWrapper;
+import org.usfirst.frc.team449.robot.util.YamlSubsystem;
+
+/**
+ * Created by noah on 6/24/17.
+ */
+public class DetermineNominalVoltage <T extends YamlSubsystem & UnidirectionalDrive> extends YamlCommandWrapper{
+
+	/**
+	 * The drive subsystem to execute this command on.
+	 */
+	private T subsystem;
+
+	private double percentVoltage;
+
+	/**
+	 * Default constructor
+	 *
+	 * @param drive   The drive to execute this command on
+	 */
+	@JsonCreator
+	public DetermineNominalVoltage(@JsonProperty(required = true) T drive) {
+		//Initialize stuff
+		this.subsystem = drive;
+		requires(drive);
+		Logger.addEvent("Drive Robot bueno", this.getClass());
+	}
+
+	/**
+	 * Set up start time.
+	 */
+	@Override
+	protected void initialize() {
+		percentVoltage = 0;
+		//Reset drive velocity (for safety reasons)
+		subsystem.fullStop();
+		Logger.addEvent("DetermineNominalVoltage init", this.getClass());
+	}
+
+	/**
+	 * Send output to motors and log data
+	 */
+	@Override
+	protected void execute() {
+		//Adjust it by 0.01 per second, so 0.01 * 20/1000, which is 0.0002
+		percentVoltage += 0.0002;
+		//Set the velocity
+		subsystem.setOutput(percentVoltage, percentVoltage);
+	}
+
+	/**
+	 * Exit after the command's been running for long enough
+	 *
+	 * @return True if timeout has been reached, false otherwise
+	 */
+	@Override
+	protected boolean isFinished() {
+		return Math.max(subsystem.getLeftVel(), subsystem.getRightVel()) >= 0.5;
+	}
+
+	/**
+	 * Stop the drive when the command ends.
+	 */
+	@Override
+	protected void end() {
+		//Brake on exit. Yes this should be setOutput because often we'll be testing how well the PID loop handles a full stop.
+		subsystem.fullStop();
+		Logger.addEvent("DetermineNominalVoltage end.", this.getClass());
+	}
+
+	/**
+	 * Log and stop the drive when the command is interrupted.
+	 */
+	@Override
+	protected void interrupted() {
+		Logger.addEvent("DetermineNominalVoltage Interrupted! Stopping the robot.", this.getClass());
+		//Brake if we're interrupted
+		subsystem.fullStop();
+	}
+}
