fa5b9180f92c8b7b76b6d746372675274a4ad2ec
Noah Gleason <noah.gleason1@gmail.com>
2017-10-23 21:51:43 -0400

    Fix drawMP

--- R scripts/drawMP.R
+++ R scripts/drawMP.R
@@ -21,7 +21,6 @@ plotProfile <- function(profileName, inverted = FALSE, wheelbaseDiameter, center
   
   for(i in 2:length(left$V4)){
     #Get the angle the robot is facing.
-    #perpendicular <- angleBetween(leftX = out[i-1,2], leftY = out[i-1,3], rightX = out[i-1,4], rightY = out[i-1,5])-pi/2
     perpendicular <- out[i-1,6]
     print(perpendicular)
     
@@ -55,45 +54,30 @@ plotProfile <- function(profileName, inverted = FALSE, wheelbaseDiameter, center
     
     # If theta is 0, we're going straight and need to treat it as a special case.
     if (identical(theta, 0)){
-      if(inverted){
-        out[i, 2] <- out[i-1,2]+deltaRight*cos(perpendicular)
-        out[i, 3] <- out[i-1,3]+deltaRight*sin(perpendicular)
-        out[i, 4] <- out[i-1,4]+deltaLeft*cos(perpendicular)
-        out[i, 5] <- out[i-1,5]+deltaLeft*sin(perpendicular)
-      } else{
-        out[i, 2] <- out[i-1,2]+deltaLeft*cos(perpendicular)
-        out[i, 3] <- out[i-1,3]+deltaLeft*sin(perpendicular)
-        out[i, 4] <- out[i-1,4]+deltaRight*cos(perpendicular)
-        out[i, 5] <- out[i-1,5]+deltaRight*sin(perpendicular)
-      }
+      out[i, 2] <- out[i-1,2]+deltaLeft*cos(perpendicular)
+      out[i, 3] <- out[i-1,3]+deltaLeft*sin(perpendicular)
+      out[i, 4] <- out[i-1,4]+deltaRight*cos(perpendicular)
+      out[i, 5] <- out[i-1,5]+deltaRight*sin(perpendicular)
     } else {
       
       #We do this with sectors, so this is the radius of the turning circle for the
       #left and right sides. They just differ by the diameter of the wheelbase.
-      rightR <- (wheelbaseDiameter/2) * (deltaRight + deltaLeft) / diffTerm - wheelbaseDiameter/2
-      leftR <- rightR + wheelbaseDiameter
+      rightR <- (wheelbaseDiameter/2) * (deltaRight + deltaLeft) / diffTerm + wheelbaseDiameter/2
+      leftR <- rightR - wheelbaseDiameter
       
       #This is the angle for the vector pointing towards the new position of each
       #wheel.
       #To understand why this formula is correct, overlay isoclese triangles on the sectors
-      vectorTheta <- perpendicular+theta/2
+      vectorTheta <- (out[i-1,6]+out[i,6])/2
       
       #The is the length of the vector pointing towards the new position of each
       #wheel divided by the radius of the turning circle.
       vectorDistanceWithoutR <- 2*sin(theta/2)
-      
-      if(inverted){
-        out[i, 2] <- out[i-1,2]+vectorDistanceWithoutR*rightR*cos(vectorTheta)
-        out[i, 3] <- out[i-1,3]+vectorDistanceWithoutR*rightR*sin(vectorTheta)
-        out[i, 4] <- out[i-1,4]+vectorDistanceWithoutR*leftR*cos(vectorTheta)
-        out[i, 5] <- out[i-1,5]+vectorDistanceWithoutR*leftR*sin(vectorTheta)
-      } else {
-        out[i, 2] <- out[i-1,2]+vectorDistanceWithoutR*leftR*cos(vectorTheta)
-        out[i, 3] <- out[i-1,3]+vectorDistanceWithoutR*leftR*sin(vectorTheta)
-        out[i, 4] <- out[i-1,4]+vectorDistanceWithoutR*rightR*cos(vectorTheta)
-        out[i, 5] <- out[i-1,5]+vectorDistanceWithoutR*rightR*sin(vectorTheta)
-      }
     
+      out[i, 2] <- out[i-1,2]+vectorDistanceWithoutR*leftR*cos(vectorTheta)
+      out[i, 3] <- out[i-1,3]+vectorDistanceWithoutR*leftR*sin(vectorTheta)
+      out[i, 4] <- out[i-1,4]+vectorDistanceWithoutR*rightR*cos(vectorTheta)
+      out[i, 5] <- out[i-1,5]+vectorDistanceWithoutR*rightR*sin(vectorTheta)
     }
   }
   return(out)
@@ -103,9 +87,9 @@ drawProfile <- function (coords, centerToFront, centerToBack, wheelbaseDiameter,
   
   if (clear){
     if (linePlot){
-      plot(coords[,2],coords[,3], type="l", col="Green", ylim=c(-16, 16),xlim = c(0,54), asp=1)
+      plot(coords[,2],coords[,3], type="l", col="Green", ylim=c(-16, 16),xlim = c(0,54), xlab = "X Position (feet)", ylab="Y Position (feet)", asp=1)
     } else {
-      plot(coords[,2],coords[,3], col="Green", ylim=c(-16, 16), xlim = c(0,54), asp=1)
+      plot(coords[,2],coords[,3], col="Green", ylim=c(-16, 16), xlim = c(0,54), xlab = "X Position (feet)", ylab="Y Position (feet)", asp=1)
     }
     field <- read.csv("field.csv")
     #Strings are read as factors by default, so we need to do this to make it read them as strings
@@ -156,11 +140,11 @@ out <- plotProfile(profileName = "RedRight", inverted = FALSE, wheelbaseDiameter
 drawProfile(coords=out, centerToFront=centerToFront, centerToBack=centerToBack, wheelbaseDiameter = wheelbaseDiameter, clear = TRUE, linePlot = TRUE)
 tmp <- out[length(out[,1]),]
 drawRobot("robot.csv", tmp)
-out2 <- plotProfile(profileName = "RedShoot",inverted = TRUE,wheelbaseDiameter =  wheelbaseDiameter,centerToFront = centerToFront,centerToBack = centerToBack,centerToSide = centerToSide,startPos = tmp)
+out2 <- plotProfile(profileName = "RedBackup",inverted = TRUE,wheelbaseDiameter =  wheelbaseDiameter,centerToFront = centerToFront,centerToBack = centerToBack,centerToSide = centerToSide,startPos = tmp)
 drawProfile(coords = out2, centerToFront = centerToFront, centerToBack = centerToBack, wheelbaseDiameter = wheelbaseDiameter, clear = FALSE)
 tmp2 <- out2[length(out2[,1]),]
 drawRobot("robot.csv", out2[length(out2[,1]),])
-out3 <- plotProfile(profileName = "BlueLoadingToLoading",inverted = FALSE,wheelbaseDiameter =  wheelbaseDiameter,centerToFront = centerToFront,centerToBack = centerToBack,centerToSide = centerToSide,startPos = tmp2)
+out3 <- plotProfile(profileName = "RedBoilerToLoading",inverted = FALSE,wheelbaseDiameter =  wheelbaseDiameter,centerToFront = centerToFront,centerToBack = centerToBack,centerToSide = centerToSide,startPos = tmp2)
 drawProfile(coords = out3, centerToFront = centerToFront, centerToBack = centerToBack, wheelbaseDiameter = wheelbaseDiameter, clear = FALSE)
 drawRobot("robot.csv", out3[length(out3[,1]),])
 
