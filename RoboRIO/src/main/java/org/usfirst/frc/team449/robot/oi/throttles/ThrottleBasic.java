cadacef0b00df6f17133567a5dfe7c85a61bfcce
Noah Gleason <noah.gleason1@gmail.com>
2017-10-27 22:40:41 -0400

    re-add smoothing

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/oi/throttles/ThrottleBasic.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/oi/throttles/ThrottleBasic.java
@@ -5,14 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import org.jetbrains.annotations.NotNull;
 import org.usfirst.frc.team449.robot.jacksonWrappers.MappedJoystick;
+import org.usfirst.frc.team449.robot.other.Clock;
+
+import java.util.Random;
 
 /**
  * A class representing a single axis on a joystick.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
-public class ThrottleBasic implements Throttle {
+public class ThrottleBasic implements Throttle, PIDSource{
 
 	/**
 	 * The stick we're using
@@ -29,6 +34,8 @@ public class ThrottleBasic implements Throttle {
 	 * Whether or not the controls should be inverted
 	 */
 	private final boolean inverted;
+
+	private Random r = new Random();
 
 	/**
 	 * Default constructor.
@@ -52,6 +59,36 @@ public class ThrottleBasic implements Throttle {
 	 * @return The raw joystick output, on [-1, 1].
 	 */
 	public double getValue() {
+		return (inverted ? -1 : 1) * stick.getRawAxis(axis);
+	}
+
+	/**
+	 * Set which parameter of the device you are using as a process control variable.
+	 *
+	 * @param pidSource An enum to select the parameter.
+	 */
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		//Do nothing
+	}
+
+	/**
+	 * Get which parameter of the device you are using as a process control variable.
+	 *
+	 * @return the currently selected PID source parameter
+	 */
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return null;
+	}
+
+	/**
+	 * Get the result to use in PIDController.
+	 *
+	 * @return the result to use in PIDController
+	 */
+	@Override
+	public double pidGet() {
 		return (inverted ? -1 : 1) * stick.getRawAxis(axis);
 	}
 }
